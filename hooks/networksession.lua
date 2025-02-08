3657fa88bec9955efac46ffc648096f82bb0cbc5
offyerrocker <offyerrocker88@gmail.com>
2025-02-07 17:37:07 -0800

    fixed network handshake event, again
    turns out those functions were not identical. so much for concise code

--- hooks/networksession.lua
+++ hooks/networksession.lua
@@ -1,13 +1,16 @@
-local function on_peer_synced(self,peer_id)
-	if QuickChat then
-		QuickChat:SendSyncPeerVersionToAll()
-		QuickChat:SendAllMyWaypointsToPeer(peer_id)
-	end
-end
-
 local required_script = string.lower(RequiredScript)
 if required_script == "lib/network/base/clientnetworksession" then
-	Hooks:PostHook(ClientNetworkSession,"on_peer_synched","QuickChat_ClientNetworkSession_onpeersynched",on_peer_synced)
+	Hooks:PostHook(ClientNetworkSession,"on_peer_synched","QuickChat_ClientNetworkSession_onpeersynched",function(self,peer_id)
+		if QuickChat then
+			QuickChat:SendSyncPeerVersionToAll()
+			QuickChat:SendAllMyWaypointsToPeer(peer_id)
+		end
+	end)
 elseif required_script == "lib/network/base/hostnetworksession" then
-	Hooks:PostHook(HostNetworkSession,"on_peer_sync_complete","QuickChat_HostNetworkSession_onpeersynccomplete",on_peer_synced)
+	Hooks:PostHook(HostNetworkSession,"on_peer_sync_complete","QuickChat_HostNetworkSession_onpeersynccomplete",function(self,peer,peer_id)
+		if QuickChat then
+			QuickChat:SendSyncPeerVersionToAll()
+			QuickChat:SendAllMyWaypointsToPeer(peer_id)
+		end
+	end)
 end
